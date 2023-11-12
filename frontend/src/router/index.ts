cabf2e983e796eef86695d60905d9de1a21e6235
Vinicius Martins <martinsrvinicius@gmail.com>
2023-11-12 22:37:09 +0000

    router test change

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -79,8 +79,8 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  //history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory('/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory('/'),
 
   //  mode: process.env.VUE_APP_ROUTER_MODE_HISTORY === 'true' ? 'history' : 'hash',
   routes,
