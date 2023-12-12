20bb7c9ac2bb023e51f683f425a95ad9ac7142f4
Vinicius Martins <martinsrvinicius@gmail.com>
2023-12-12 23:11:15 +0000

    Create client full info
    Backend

--- backend/core/client.php
+++ backend/core/client.php
@@ -30,7 +30,7 @@ class Client
         return $stmt;
     }
 
-    //Insert new event on calendar
+    //Insert client 
     public function create()
     {
         //create query
@@ -54,6 +54,44 @@ class Client
         return false;
     }
 
+    //Insert new client full info
+    public function createFull($obj)
+    {
+        //create query
+        $query = 'INSERT INTO `cliente`(uniqueId, nome, data_nasc, telefone, whatsapp, rua, localidade, zipcode, pais) VALUES (:uniqueId, :nome, :data_nasc, :telefone, :whatsapp, :rua, :localidade, :zipcode, :pais)';
+        //prepare statement
+        $stmt = $this->conn->prepare($query);
+        //clean data
+       $pais = htmlspecialchars(strip_tags($obj->pais));
+        $zipcode = htmlspecialchars(strip_tags($obj->zipcode));
+        $localidade = htmlspecialchars(strip_tags($obj->localidade));
+        $rua = htmlspecialchars(strip_tags($obj->rua));
+        $whatsapp = htmlspecialchars(strip_tags($obj->whatsapp));
+        $telefone = htmlspecialchars(strip_tags($obj->telefone));
+        $data_nasc = htmlspecialchars(strip_tags(date('Y-m-d', strtotime($obj->data_nasc))));
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->uniqueId = htmlspecialchars(strip_tags($this->getUnique($this->name)));
+
+        //binding parameters
+        $stmt->bindParam(':pais', $pais);
+        $stmt->bindParam(':zipcode', $zipcode);
+        $stmt->bindParam(':localidade', $localidade);
+        $stmt->bindParam(':rua', $rua);
+        $stmt->bindParam(':whatsapp', $whatsapp);
+        $stmt->bindParam(':telefone', $telefone);
+        $stmt->bindParam(':data_nasc', $data_nasc);
+        $stmt->bindParam(':uniqueId', $this->uniqueId);
+        $stmt->bindParam(':nome', $this->name);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        //print error if something goes wrong
+        printf("Error %s \n", $stmt->error);
+        return false;
+    }
+    
 
     //Creates uniqueId 
     public function getUnique($userName)
