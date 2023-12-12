7eadb49034189be4ada65f88ce19848f916f407b
Vinicius Martins <martinsrvinicius@gmail.com>
2023-12-12 23:12:17 +0000

    Update Client by id
    Backend

---
+++ backend/api/clients/update_client.php
@@ -0,0 +1,43 @@
+<?php
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access Control-Allow-Methods, Authorization, X-Requested-With');
+
+
+//initializing our api
+include_once('../../core/initialize.php');
+
+//instantiate client
+$client = new Client($db);
+
+//get raw posted data
+$data = json_decode(file_get_contents('php://input'));
+
+$client->name = $data->name; 
+$client->uniqueId = $data->uniqueId; 
+
+$obj = (object) [
+    'data_nasc'=>null,
+    'telefone'=>null,
+    'whatsapp'=>null,
+    'rua'=>null,
+    'localidade'=>null,
+    'zipcode'=>null,
+    'pais'=>null,
+];
+$obj->data_nasc = $data->data_nasc;
+$obj->telefone = $data->telefone;
+$obj->whatsapp = $data->whatsapp;
+$obj->rua = $data->rua;
+$obj->localidade = $data->localidade;
+$obj->zipcode = $data->zipcode;
+$obj->pais = $data->pais;
+//echo json_encode($client);
+//create event on calendar
+if($client->update($obj)) {
+    echo json_encode(array($res['status'] = true));
+} else {
+    echo json_encode(array($res['status'] = false));
+}
--- backend/core/client.php
+++ backend/core/client.php
@@ -93,6 +93,47 @@ class Client
     }
     
 
+//update client 
+public function update($obj)
+{
+    $query = 'UPDATE `cliente` SET nome=:nome, 
+    data_nasc=:data_nasc, telefone=:telefone, whatsapp=:whatsapp, rua=:rua, localidade=:localidade, zipcode=:zipcode, pais=:pais WHERE uniqueId=:uniqueId';
+
+    //prepare statement
+    $stmt = $this->conn->prepare($query);
+    //clean data
+    $pais = htmlspecialchars(strip_tags($obj->pais));
+    $zipcode = htmlspecialchars(strip_tags($obj->zipcode));
+    $localidade = htmlspecialchars(strip_tags($obj->localidade));
+    $rua = htmlspecialchars(strip_tags($obj->rua));
+    $whatsapp = htmlspecialchars(strip_tags($obj->whatsapp));
+    $telefone = htmlspecialchars(strip_tags($obj->telefone));
+    $data_nasc = htmlspecialchars(strip_tags(date('Y-m-d', strtotime($obj->data_nasc))));
+    $this->name = htmlspecialchars(strip_tags($this->name));
+    $this->uniqueId = htmlspecialchars(strip_tags($this->uniqueId));
+
+    //binding parameters
+    $stmt->bindParam(':pais', $pais);
+    $stmt->bindParam(':zipcode', $zipcode);
+    $stmt->bindParam(':localidade', $localidade);
+    $stmt->bindParam(':rua', $rua);
+    $stmt->bindParam(':whatsapp', $whatsapp);
+    $stmt->bindParam(':telefone', $telefone);
+    $stmt->bindParam(':data_nasc', $data_nasc);
+    $stmt->bindParam(':uniqueId', $this->uniqueId);
+    $stmt->bindParam(':nome', $this->name);
+
+    //execute the query
+    if ($stmt->execute()) {
+        return true;
+    }
+
+    //print error if something goes wrong
+    printf("Error %s \n", $stmt->error);
+    return false;
+}
+
+
     //Creates uniqueId 
     public function getUnique($userName)
     {
